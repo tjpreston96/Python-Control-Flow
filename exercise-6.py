f4c2ffc1fc1150e396823233586cd51d0ca99927
tpreston96 <tpreston96@gmail.com>
2021-01-26 22:49:42 -0600

    code fix

--- exercise-6.py
+++ exercise-6.py
@@ -3,7 +3,7 @@
 # Write the code that:
 # 1. Prompts the user to enter the month (as three characters):
 #      Enter the month of the year (Jan - Dec):
-# 2. Then prompts the user to enter the day of the month: 
+# 2. Then prompts the user to enter the day of the month:
 #      Enter the day of the month:
 # 3. Calculate what season it is based upon this chart:
 #      Dec 21 - Mar 19: Winter
@@ -11,7 +11,7 @@
 #      Jun 21 - Sep 21: Summer
 #      Sep 22 - Dec 20: Fall
 # 4. Print the result as follows:
-#      <Mmm> <dd> is in <season> 
+#      <Mmm> <dd> is in <season>
 
 # Hints:
 # Consider using the in operator to check if a string is in a particular list/tuple like this:
@@ -25,22 +25,20 @@
 month = input("Enter the month of the year (Jan - Dec): ")
 day = int(input("Enter the day of the month: "))
 
-if month in ('Jan',"Feb","Mar"):
-  season = 'Winter'
-elif month in ('Apr','May','Jun'):
-  season = 'Spring'
-elif month in ('Jul', 'Aug','Sep'):
-  season = 'Summer'
+if month in ("Jan", "Feb", "Mar"):
+    season = "Winter"
+elif month in ("Apr", "May", "Jun"):
+    season = "Spring"
+elif month in ("Jul", "Aug", "Sep"):
+    season = "Summer"
 else:
-  season = 'Fall'
-if month == 'Mar' and day > 19:
-  season = 'Spring'
-elif month == 'Jun' and day > 20:
-  season = 'Summer'
-elif month == 'Sep' and day > 21:
-  season = 'Fall'
-elif month == 'Dec' and day > 20:
-  season = 'Winter'
-print(f'This date falls in {season}')
-
-
+    season = "Fall"
+if month == "Mar" and day > 20:
+    season = "Spring"
+elif month == "Jun" and day > 20:
+    season = "Summer"
+elif month == "Sep" and day > 22:
+    season = "Fall"
+elif month == "Dec" and day > 21:
+    season = "Winter"
+print(f"This date falls in {season}")
